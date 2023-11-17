21fa7cd7845477baa0670daf3136e9d346e130fe
Andrzej Herman <andrzejek.herman@gmail.com>
2023-11-17 12:58:18 +0100

    Add project

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home()   {
+  return (
+    <div className="h-full flex items-center justify-center">
+      <h1 className="text-5xl text-emerald-600 tracking-tighter">Welcome to McDonalds</h1>
+    </div>
+  )
+}
